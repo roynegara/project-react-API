f97381559dbba6d0247ee926b7544b0e574a9ea9
roynegara <roybayunegara92@gmail.com>
2024-03-08 10:25:34 +0700

    add list and add detail   without pagination

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -15,19 +15,53 @@ const User = () => {
   const [activePage, setActivePage] = useState(1);
 
   const getUsersData = () => {
-    axios.get(`https://reqres.in/api/users?page=${pagination.page}`).then((res) => {
-      console.log(res);
-      setUsers(res.data.data);
+    axios
+      .get(`https://reqres.in/api/users?page=${pagination.page}`)
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data.data);
         setPagination({
-            page: res.data.page,
-            per_page: res.data.per_page,
-            total: res.data.total,
-            total_pages: res.data.total_pages
+          page: res.data.page,
+          per_page: res.data.per_page,
+          total: res.data.total,
+          total_pages: res.data.total_pages,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    });
   };
 
-  return <h1>User</h1>;
+  useEffect(() => {
+    getUsersData();
+  }, []);
+
+  return (
+    <Layout>
+      {" "}
+      <div>
+        <h1>User</h1>
+        <div>
+          {users.map((item, index) => (
+            <div key={index}>
+                  <h1>{item.first_name} {item.last_name}</h1>
+                  <img src={item.avatar} />
+
+              <div>
+                <Link to={`/user/${item.id}`}>
+                  <button>Detail</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+          <div>
+            <button>Back</button>
+            <button>Next</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default User;
