0d373ce6cfdbc8881edafa129778d7078f8abb8b
roynegara <roybayunegara92@gmail.com>
2024-03-09 07:31:43 +0700

    add my Logo it is Project RBN in the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,30 +5,30 @@ const Footer = () => {
     <footer>
       <div className="footer">
         <div className="footer-left">
-          <img src="https://avatars.githubusercontent.com/u/84184940?v=4" alt="Project RBN Logo" />
+          <img src="./src/assets/img/logo5.png" alt="Project RBN Logo" />
           {/* <p>Project RBN. Company Pte. Ltd</p> */}
         </div>
         <div className="footer-center">
           <p>Opening Hours:</p>
-          <p>Monday - Friday: 8:00AM - 5:00PM</p>
+          <p>Monday - Friday : 8:00AM - 5:00PM</p>
 
-          <p>Saturday - Sunday: Closed</p>
+          <p>Saturday - Sunday : Closed</p>
         </div>
         <div className="footer-right">
           <form>
-            <input type="text" placeholder="Nama" />
+            <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
             <button type="submit">Send</button>
           </form>
           <div className="social-icons">
-           <p>Follow us: </p>
-                      <a href="#">
+            <p>Follow us: </p>
+            <a href="https://www.instagram.com">
               <i className="bx bxl-instagram"></i>
             </a>
-            <a href="#">
+            <a href="https://www.twitter.com">
               <i className="bx bxl-twitter"></i>
             </a>
-            <a href="#">
+            <a href="https://wa.me/+62123456789">
               <i className="bx bxl-whatsapp"></i>
             </a>
           </div>
