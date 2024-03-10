0174528df211a85e89665021adb8d2343067ea97
roynegara <roybayunegara92@gmail.com>
2024-03-10 08:42:47 +0700

    repair home page to clean code at index.jsx and better looking for UI

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,41 +1,16 @@
 import Layout from "../../components/Layout";
-import { Link, useNavigate } from "react-router-dom";
-import LogInOut from "../../components/LogInOut/LogInOut";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const token = localStorage.getItem("access_token");
-
-  console.log("access_token", token);
-
-  const handleLogout = () => {
-    localStorage.removeItem("access_token");
-    navigate("/login");
-  };
-
   return (
     <Layout>
       <div className="home">
-        <h1 className="home-title">Welcome to Project RBN. Website</h1>
-        <div className="home-content">
-          <p className="home-description-1">
-            Our company offers excellent consulting services with an experienced team of experts in Project RBN. With a
-            professional approach and innovative solutions. We are ready to assist customers in achieving their goals.
-            Join us to obtain optimal results for your business.
-          </p>
-          <div className="home-description-2">
-            {" "}
-            <p>If you are interested in joining, please register here.</p>
-            <button className="home-btn">
-              <Link to="/register">Register</Link>
-            </button>
-            <p>And if you have already registered, please log in here.</p>
-            <div className="home-btn-loginout">
-              <LogInOut />
-            </div>
-          </div>
-        </div>
+        <p className="home-title">Project RBN. Website</p>        
+          <h1 className="home-description-1">Excellent consulting services with an experienced team of experts.</h1>
+          <button className="home-btn">
+            <Link to="/register">Join Us</Link>
+          </button>        
       </div>
     </Layout>
   );
