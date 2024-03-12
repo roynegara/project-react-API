8f4e1d9330884bb8056134ce38cca0aadc4c04e2
roynegara <roybayunegara92@gmail.com>
2024-03-12 10:46:34 +0700

    repair bug Protected Login alert not appear, now appear

--- src/Route/ProtectedLogin.jsx
+++ src/Route/ProtectedLogin.jsx
@@ -15,21 +15,15 @@
 
 import { Navigate, Outlet } from "react-router-dom";
 import alertHandler from "../components/alertHandler";
-import { useEffect } from "react";
 
-export const ProtectedLogin = ({ children }) => {
+const ProtectedLogin = ({ children }) => {
   const token = localStorage.getItem("access_token");
-
-  useEffect(() => {
-    // Trigger the alert only once when the component mounts
-    if (token) {
-      alertHandler.alertProtectedLogin();
-    }
-  }, []); // Empty dependency array ensures the effect runs only once
-
   if (token) {
+    alert(alertHandler.alertProtectedLogin());
     return <Navigate to="/" />;
   } else {
     return children || <Outlet />;
   }
 };
+
+export default ProtectedLogin;
